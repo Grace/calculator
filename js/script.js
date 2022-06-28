90ef6ce5259d481ab96c2d97549a5f2782ec8cc8
Grace <grace@gracefulco.de>
2022-06-28 18:35:02 -0400

    Implement HTML+CSS for calculator.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y === 0) return null;
+    return x / y;
+}
+
+function operate(operator, x, y) {
+    switch(operator) {
+       case '+': return add(x, y);
+       case '-': return subtract(x, y);
+       case '*': return multiply(x, y);
+       case '/': return divide(x, y);
+       default: return null;
+    }
+}

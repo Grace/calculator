e4b342329f735248e31f5da0f60dd656a4076dc8
Grace <grace@gracefulco.de>
2022-06-28 21:10:34 -0400

    Refactor code to be more readable.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
 function add(x, y) {
-    return x + y;
+    return Number(x + y);
 }
 
 function subtract(x, y) {
-    return x - y;
+    return Number(x - y);
 }
 
 function multiply(x, y) {
-    return x * y;
+    return Number(x * y);
 }
 
 function divide(x, y) {
     if (y === 0) return 'NaN. Press C.';
-    return x / y;
+    return Number(x / y);
 }
 
 function modulus(x, y) {
-    return x % y;
+    return Number(x % y);
 }
 
 function operate(operator, x, y) {
-    if (operator === null) {
-        return 'Err: null operator';
-    }
     switch(operator.toString()) {
        case '+': return add(x, y);
        case '-': return subtract(x, y);
@@ -33,21 +30,60 @@ function operate(operator, x, y) {
     }
 }
 
+let globals = {
+    input: '',
+    x: 0,
+    y: 0,
+    operator: null,
+    operatorPressedCount: 0
+}
+
 var input = '';
 var x = 0;
 var y = 0;
 var operator = null;
 var operatorPressedCount = 0;
 
+const setX = (n) => x = n;
+const setY = (n) => y = n;
+const resetXY = () => {
+    x = 0;
+    y = 0;
+}
+const setOperator = (newOperator) => operator = newOperator;
+const getOperator = () => operator;
+const resetOperator = () => operator = null;
+const resetOperatorPressedCount = () => operatorPressedCount = 0;
+const incrementOperatorPressedCount = () => operatorPressedCount++;
+const getOperatorPressedCount = () => operatorPressedCount;
+
 function updateDisplay(value) {
+    console.log(`value: ${value}, type: ${typeof value}`);
     let display = document.querySelector('.display');
-    display.textContent = value.toString();
+    // If value is a whole number, display it as an Integer
+    if (value % 1 === 0) {
+        display.textContent = parseInt(value).toString();
+    } else { // Otherwise fix to 1 decimal place
+        display.textContent = value.toFixed(1);
+    }
+}
+
+function addToInput(textNumber) {
+    let number = parseInt(textNumber);
+    input = Number((input.toString()).concat(textNumber.toString()));
+}
+
+function getInputAsNumber() {
+    return Number(input);
+}
+
+function resetInput() {
+    input = '';
 }
 
 function numberPressed(e) {
-    let number = parseInt(e.target.textContent);
-    input = input.concat(number);
-    updateDisplay(input);
+    addToInput(e.target.textContent);
+    updateDisplay(getInputAsNumber());
 }
 
 numberDivs = document.querySelectorAll('.number');
@@ -55,28 +91,27 @@ numberDivs.forEach(div => div.addEventListener('click', numberPressed));
 
 function operatorPressed(e) {
     if (operatorPressedCount < 1) {
-        operator = e.target.textContent;
+        setOperator(e.target.textContent)
         console.log(`operator: ${operator}`);
         console.log(`current input: ${input}`);
-        x = parseInt(input);
-        input = '';
+        setX(getInputAsNumber());
+        resetInput();
     } else {
-        y = parseInt(input);
+        setY(getInputAsNumber());
         console.log(`current input: ${input}`);
         let lastOperator = operator;
         console.log(`last operator: ${lastOperator}`);
         result = operate(lastOperator, x, y);
         updateDisplay(result);
         console.log(`result: ${result}`);
-        let newOperator = e.target.textContent;
-        operator = newOperator;
-        console.log(`new operator: ${newOperator}`);
-        x = result;
+        setOperator(e.target.textContent);
+        console.log(`new operator: ${e.target.textContent}`);
+        setX(result);
         console.log(`new x (result): ${x}`);
-        y = 0;
-        input = '';
-        operatorPressedCount = 0;
-        console.log(`operatorPressedCount: ${operatorPressedCount}`);
+        setY(0);
+        resetInput();
+        resetOperatorPressedCount();
+        console.log(`operatorPressedCount: ${getOperatorPressedCount()}`);
     }
     operatorPressedCount++;
 }
@@ -85,7 +120,7 @@ operatorDivs = document.querySelectorAll('.operator');
 operatorDivs.forEach(div => div.addEventListener('click', operatorPressed));
 
 function equalsPressed(e) {
-    y = parseInt(input);
+    setY(getInputAsNumber());
     result = operate(operator, x, y);
     updateDisplay(result);
     clear();
@@ -95,16 +130,15 @@ equalsDiv = document.querySelector('.equals');
 equalsDiv.addEventListener('click', equalsPressed);
 
 function clear() {
-    x = 0;
-    y = 0;
-    input = '';
-    operator = null;
-    operatorPressedCount = 0;
+    resetXY();
+    resetInput();
+    resetOperator();
+    resetOperatorPressedCount();
 }
 
 function clearPressed(e) {
     clear();
-    updateDisplay('0');
+    updateDisplay(0);
 }
 
 clearDiv = document.querySelector('.clear');

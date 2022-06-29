89c4f872262ee0dac4271721f875f4e15be84c34
Grace <grace@gracefulco.de>
2022-06-29 06:03:24 -0400

    Divide by 0 results in display of NaN.

--- js/script.js
+++ js/script.js
@@ -38,47 +38,49 @@ let globals = {
     operatorPressedCount: 0
 }
 
-var input = '';
-var x = 0;
-var y = 0;
-var operator = null;
-var operatorPressedCount = 0;
-
-const setX = (n) => x = n;
-const setY = (n) => y = n;
+const getX = () => globals.x;
+const getY = () => globals.y;
+const setX = (n) => globals.x = n;
+const setY = (n) => globals.y = n;
 const resetXY = () => {
-    x = 0;
-    y = 0;
+    globals.x = 0;
+    globals.y = 0;
 }
-const setOperator = (newOperator) => operator = newOperator;
-const getOperator = () => operator;
-const resetOperator = () => operator = null;
-const resetOperatorPressedCount = () => operatorPressedCount = 0;
-const incrementOperatorPressedCount = () => operatorPressedCount++;
-const getOperatorPressedCount = () => operatorPressedCount;
+const setOperator = (newOperator) => globals.operator = newOperator;
+const getOperator = () => globals.operator;
+const resetOperator = () => globals.operator = null;
+const resetOperatorPressedCount = () => globals.operatorPressedCount = 0;
+const incrementOperatorPressedCount = () => globals.operatorPressedCount++;
+const getOperatorPressedCount = () => globals.operatorPressedCount;
 
-function updateDisplay(value) {
+const updateDisplay = (value) => {
     console.log(`value: ${value}, type: ${typeof value}`);
     let display = document.querySelector('.display');
-    // If value is a whole number, display it as an Integer
-    if (value % 1 === 0) {
-        display.textContent = parseInt(value).toString();
-    } else { // Otherwise fix to 1 decimal place
-        display.textContent = value.toFixed(1);
+    if(typeof value === 'string') {
+        console.log('string type in display function: ' + value);
+        document.querySelector('.display').textContent = 'NaN';
+    } else if (typeof value === 'number') {
+        console.log('number type in display function: ' + value);
+        if (value % 1 === 0) { // If value is a whole number, display it as an Integer
+            display.textContent = parseInt(value).toString();
+        } else { // Otherwise fix to 1 decimal place
+            display.textContent = value.toFixed(1);
+        }
     }
 }
 
 function addToInput(textNumber) {
-    let number = parseInt(textNumber);
-    input = Number((input.toString()).concat(textNumber.toString()));
+    globals.input = Number((globals.input.toString()).concat(textNumber.toString()));
 }
 
+const getInput = () => globals.input;
+
 function getInputAsNumber() {
-    return Number(input);
+    return Number(globals.input);
 }
 
 function resetInput() {
-    input = '';
+    globals.input = '';
 }
 
 function numberPressed(e) {
@@ -90,39 +92,50 @@ numberDivs = document.querySelectorAll('.number');
 numberDivs.forEach(div => div.addEventListener('click', numberPressed));
 
 function operatorPressed(e) {
-    if (operatorPressedCount < 1) {
+    let newOperator = e.target.textContent;
+    if (getOperatorPressedCount() < 1) {
         setOperator(e.target.textContent)
-        console.log(`operator: ${operator}`);
-        console.log(`current input: ${input}`);
+        console.log(`operator: ${getOperator()}`);
+        console.log(`current input (x): ${getInputAsNumber()}`);
         setX(getInputAsNumber());
         resetInput();
     } else {
         setY(getInputAsNumber());
-        console.log(`current input: ${input}`);
-        let lastOperator = operator;
-        console.log(`last operator: ${lastOperator}`);
-        result = operate(lastOperator, x, y);
+        console.log(getY());
+        console.log(`current input (y): ${getInputAsNumber()}`);
+        console.log(`last operator: ${getOperator()}`);
+        result = operate(getOperator(), getX(), getY());
         updateDisplay(result);
         console.log(`result: ${result}`);
+        let newOperator = e.target.textContent;
+        console.log(`new oeprator ${e.target.textContent}`);
         setOperator(e.target.textContent);
-        console.log(`new operator: ${e.target.textContent}`);
         setX(result);
-        console.log(`new x (result): ${x}`);
+        console.log(`new x (result): ${getX()}`);
         setY(0);
         resetInput();
         resetOperatorPressedCount();
         console.log(`operatorPressedCount: ${getOperatorPressedCount()}`);
     }
-    operatorPressedCount++;
+    incrementOperatorPressedCount();
 }
 
 operatorDivs = document.querySelectorAll('.operator');
 operatorDivs.forEach(div => div.addEventListener('click', operatorPressed));
 
 function equalsPressed(e) {
-    setY(getInputAsNumber());
-    result = operate(operator, x, y);
-    updateDisplay(result);
+    console.log(globals.input);
+    console.log(globals.operator);
+    console.log(`input: ${getInput()}`);
+    if(getInput() === 0 && getOperator() === '/') {
+        console.log('input is 0 and operator is /');
+        updateDisplay('NaN');
+        // document.querySelector('.display').textContent = 'NaN';
+    } else {
+        getY(getInputAsNumber());
+        result = operate(getOperator(), getX(), getY());
+        updateDisplay(result);
+    }
     clear();
 }
 

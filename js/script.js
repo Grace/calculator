9ac07ae5b413ab85f00cfcf920170b197a34630f
Grace <grace@gracefulco.de>
2022-07-03 21:32:35 -0400

    Change clickable divs to button elements and add ARIA labels to improve accessibility.

--- js/script.js
+++ js/script.js
@@ -1,158 +1,179 @@
-function add(x, y) {
+// Variable declarations
+let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+const calculatorButtons = document.querySelectorAll('button.calculator-button');
+const expressionDisplay = document.querySelector('#expression');
+const display = document.querySelector('#display');
+
+window.addEventListener('keydown', function(e) {
+    console.log(e.key);
+    let key = e.key.toLowerCase();
+    let btn = document.querySelector(`button[data-key='${key}']`);
+    if (btn) {
+        btn.click();
+        console.log(btn);
+    }
+});
+
+const updateDisplay = () => {
+    const display = document.querySelector('#display');
+    display.textContent = displayValue;
+    if (displayValue.length > 9) {
+        display.textContent= displayValue.substring(0, 9);
+    }
+};
+
+updateDisplay();
+
+function setupClickHandler() {
+    calculatorButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+                //Debugging key presses:
+                console.log(e.target.textContent.toLowerCase());
+                // Execute functions depending on button type
+                if(button.classList.contains('operand')) {
+                    inputOperand(button.textContent);
+                    updateDisplay();
+                } else if(button.classList.contains('operator')) {
+                    inputOperator(button.textContent);
+                } else if(button.classList.contains('equals')) {
+                    inputEquals();
+                    updateDisplay();
+                } else if(button.classList.contains('decimal')) {
+                    // inputDecimal(calculatorInput);
+                    updateDisplay();
+                } else if(button.classList.contains('percent')) {
+                    inputPercent();
+                    updateDisplay();
+                } else if(button.classList.contains('sign')) {
+                    inputSign();
+                    updateDisplay();
+                } else if(button.classList.contains('clear'))
+                    clearDisplay();
+                    updateDisplay();
+            }
+        )
+    }
+)};
+
+setupClickHandler();
+
+const inputEquals = () => {
+    if (firstOperator === null) {
+        // Do nothing
+    } else if (firstOperator && !secondOperator) {
+        result = operate(firstOperator, firstOperand, secondOperand);
+        console.log(`result ${result}`);
+        displayValue = result;
+        firstOperand = result;
+        secondOperand = null;
+        firstOperator = null;
+        secondOperator = null;
+        result = null;
+        expressionDisplay.textContent = '';
+    }
+    console.log(`1st operator ${firstOperator}`);
+    console.log(`2nd operator ${secondOperator}`);
+    console.log(`1st operand ${firstOperand}`);
+    console.log(`2nd operand ${secondOperand}`);
+}
+
+const inputOperator = (operator) => {
+    if (firstOperator === null && secondOperator === null) {
+        // 2nd click
+        firstOperator = operator;
+        firstOperand = displayValue;
+        expressionDisplay.textContent += firstOperator;
+    } else if (firstOperator && secondOperator === null) {
+        // 2nd operator clicked
+        secondOperator = operator;
+        secondOperand = displayValue;
+    } else {
+        console.log('1st operator and 2nd operator equal non-null values');
+    }
+    console.log(`1st operator ${firstOperator}`);
+    console.log(`operator ${operator}`);
+    console.log(`2nd operator ${secondOperator}`);
+    console.log(`1st operand ${firstOperand}`);
+    console.log(`2nd operand ${secondOperand}`);
+}
+
+const inputOperand = (operand) => {
+    if (firstOperand === null) {
+        if (displayValue == 0) {
+            // 1st click
+            firstOperand = operand;
+            displayValue = operand;
+            expressionDisplay.textContent += operand;
+        }
+    } else if (firstOperand !== null && secondOperand === null) {
+        if (firstOperator !== null) {
+            // 3rd click
+            secondOperand = operand;
+            displayValue = operand;
+            expressionDisplay.textContent += operand;
+        } else {
+            displayValue += operand;
+            expressionDisplay.textContent += operand;
+        }
+    } else {
+        secondOperand += operand;
+        displayValue += operand;
+        expressionDisplay.textContent += operand;
+    }
+};
+
+const inputPercent = () => {
+    displayValue *= 0.01;
+}
+
+const inputSign = () => {
+    displayValue = -displayValue;
+};
+
+const clearDisplay = () => {
+    displayValue = '0';
+    firstOperand = null;
+    secondOperand = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
+    expressionDisplay.textContent = '';
+};
+
+
+// Operation methods
+const add = (x, y) => {
     return Number(x + y);
 }
 
-function subtract(x, y) {
+const subtract = (x, y) => {
     return Number(x - y);
 }
 
-function multiply(x, y) {
+const multiply = (x, y) => {
     return Number(x * y);
 }
 
-function divide(x, y) {
-    if (y === 0) return 'NaN. Press C.';
+const divide = (x, y) => {
+    if (y === 0) {
+        return 'lol';
+    }
     return Number(x / y);
-}
+};
 
-function modulus(x, y) {
-    return Number(x % y);
-}
-
-function operate(operator, x, y) {
-    switch(operator.toString()) {
+const operate = (operator, firstOperand, secondOperand) => {
+    let x = +firstOperand;
+    let y = +secondOperand;
+    switch (operator.toString()) {
        case '+': return add(x, y);
        case '-': return subtract(x, y);
        case '*': return multiply(x, y);
        case '/': return divide(x, y);
-       case '%': return modulus(x, y);
-       default: return 'Error';
+       default: return 'NaN';
     }
-}
-
-let globals = {
-    input: '',
-    x: 0,
-    y: 0,
-    operator: null,
-    operatorPressedCount: 0
-}
-
-const getX = () => globals.x;
-const getY = () => globals.y;
-const setX = (n) => globals.x = n;
-const setY = (n) => globals.y = n;
-const resetXY = () => {
-    globals.x = 0;
-    globals.y = 0;
-}
-const setOperator = (newOperator) => globals.operator = newOperator;
-const getOperator = () => globals.operator;
-const resetOperator = () => globals.operator = null;
-const resetOperatorPressedCount = () => globals.operatorPressedCount = 0;
-const incrementOperatorPressedCount = () => globals.operatorPressedCount++;
-const getOperatorPressedCount = () => globals.operatorPressedCount;
-
-const updateDisplay = (value) => {
-    console.log(`value: ${value}, type: ${typeof value}`);
-    let display = document.querySelector('.display');
-    if(typeof value === 'string') {
-        console.log('string type in display function: ' + value);
-        document.querySelector('.display').textContent = 'NaN';
-    } else if (typeof value === 'number') {
-        console.log('number type in display function: ' + value);
-        if (value % 1 === 0) { // If value is a whole number, display it as an Integer
-            display.textContent = parseInt(value).toString();
-        } else { // Otherwise fix to 1 decimal place
-            display.textContent = value.toFixed(1);
-        }
-    }
-}
-
-function addToInput(textNumber) {
-    globals.input = Number((globals.input.toString()).concat(textNumber.toString()));
-}
-
-const getInput = () => globals.input;
-
-function getInputAsNumber() {
-    return Number(globals.input);
-}
-
-function resetInput() {
-    globals.input = '';
-}
-
-function numberPressed(e) {
-    addToInput(e.target.textContent);
-    updateDisplay(getInputAsNumber());
-}
-
-numberDivs = document.querySelectorAll('.number');
-numberDivs.forEach(div => div.addEventListener('click', numberPressed));
-
-function operatorPressed(e) {
-    let newOperator = e.target.textContent;
-    if (getOperatorPressedCount() < 1) {
-        setOperator(e.target.textContent)
-        console.log(`operator: ${getOperator()}`);
-        console.log(`current input (x): ${getInputAsNumber()}`);
-        setX(getInputAsNumber());
-        resetInput();
-    } else {
-        setY(getInputAsNumber());
-        console.log(getY());
-        console.log(`current input (y): ${getInputAsNumber()}`);
-        console.log(`last operator: ${getOperator()}`);
-        result = operate(getOperator(), getX(), getY());
-        updateDisplay(result);
-        console.log(`result: ${result}`);
-        let newOperator = e.target.textContent;
-        console.log(`new oeprator ${e.target.textContent}`);
-        setOperator(e.target.textContent);
-        setX(result);
-        console.log(`new x (result): ${getX()}`);
-        setY(0);
-        resetInput();
-        resetOperatorPressedCount();
-        console.log(`operatorPressedCount: ${getOperatorPressedCount()}`);
-    }
-    incrementOperatorPressedCount();
-}
-
-operatorDivs = document.querySelectorAll('.operator');
-operatorDivs.forEach(div => div.addEventListener('click', operatorPressed));
-
-function equalsPressed(e) {
-    console.log(globals.input);
-    console.log(globals.operator);
-    console.log(`input: ${getInput()}`);
-    if(getInput() === 0 && getOperator() === '/') {
-        console.log('input is 0 and operator is /');
-        updateDisplay('NaN');
-        // document.querySelector('.display').textContent = 'NaN';
-    } else {
-        getY(getInputAsNumber());
-        result = operate(getOperator(), getX(), getY());
-        updateDisplay(result);
-    }
-    clear();
-}
-
-equalsDiv = document.querySelector('.equals');
-equalsDiv.addEventListener('click', equalsPressed);
-
-function clear() {
-    resetXY();
-    resetInput();
-    resetOperator();
-    resetOperatorPressedCount();
-}
-
-function clearPressed(e) {
-    clear();
-    updateDisplay(0);
-}
-
-clearDiv = document.querySelector('.clear');
-clearDiv.addEventListener('click', clearPressed);
+};
